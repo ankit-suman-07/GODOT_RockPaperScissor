e456524c131318af6d7e511a0f6c3960b7954c68
Ankit Suman <ankitsuman07@gmail.com>
2026-03-20 14:33:35 -0400

    Add winning logic

--- scripts/start_game.gd
+++ scripts/start_game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const AI_OPTIONS = ["ROCK", "PAPER", "SCISSOR"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +11,34 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func select_round_winner(player_selected):
+	var probabilities = [1, 1, 1];
+	var random = RandomNumberGenerator.new()
+	var ai_selected = AI_OPTIONS[random.rand_weighted(probabilities)]
+	
+	print("Player: ", player_selected, " AI: ", ai_selected)
+	print("-----------------------------------")
+	
+	if ai_selected == player_selected:
+		print("DRAW")
+	elif ai_selected == "ROCK":
+		if player_selected == "PAPER":
+			print("PLAYER WON")
+		else:
+			print("COMPUTER WON")
+	elif ai_selected == "PAPER":
+		if player_selected == "SCISSOR":
+			print("PLAYER WON")
+		else:
+			print("COMPUTER WON")
+	else:
+		if player_selected == "ROCK":
+			print("PLAYER WON")
+		else:
+			print("COMPUTER WON")
+	
+func select_game_winner():
+	pass
 
 
 func _on_exit_btn_pressed() -> void:
@@ -18,3 +47,15 @@ func _on_exit_btn_pressed() -> void:
 
 func _on_restart_btn_pressed() -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_rock_pressed() -> void:
+	select_round_winner("ROCK")
+
+
+func _on_paper_pressed() -> void:
+	select_round_winner("PAPER")
+
+
+func _on_scissor_pressed() -> void:
+	select_round_winner("SCISSOR")
